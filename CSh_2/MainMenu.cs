9db99b0bbbe6303c6e27d66d032ae1c0b05f40ea
Alexander <dan500111@mail.ru>
2020-01-19 14:25:55 +0300

    Task_1 Complite

---
+++ CSh_2/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CSh_2
+{
+    class MainMenu
+    {
+        static void Main(string[] args)
+        {
+            int e;
+            do
+            {
+                Console.WriteLine("\n-----Упражнения к ДЗ №2-----\n" +
+                "1) Минимальное из 3-х чисел\n" +
+                "2) Количество цифр в числе\n" +
+                "8) Очистить экран\n" +
+                "Для выхода введите любой другой символ!");
+                e = int.Parse(Console.ReadLine());
+                switch (e)
+                {
+                    case 1:
+                        {
+                            Tasks.EnteringNum();
+                            break;
+                        }
+                    case 2: break;
+                    case 3: break;
+                    case 4: break;
+                    case 5: break;
+                    case 6: break;
+                    case 7: break;
+                    case 8: Console.Clear(); break;
+                }
+            } while (e >= 1 && e <= 8);
+        }
+    }
+}
---
+++ CSh_2/Tasks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSh_2
+{
+    class Tasks
+    {
+        /// <summary>
+        /// Минимальное из 3-х целых чисел
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        static int MinNum(int a, int b, int c)
+        {
+            if (a < b && a < c) return a;
+            else if (b < c) return b;
+            else return c;
+        }
+        public static void EnteringNum()
+        {
+            Console.Write("Введите 1-е число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите 2-е число: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("ВВедите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Минимальное из этих чисел: {MinNum(a, b, c)}");
+        }
+    }
+}
